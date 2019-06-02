f482f801239c3b8ccf199aa3c2d3edfbf7fddca9
zfh <fuhongzuo@gmail.com>
2019-06-02 22:23:16 +0800

    sentinel集成dubbo限流

--- bulb-sentinel-boot/src/main/java/com/maxzuo/controller/Rest.java
+++ bulb-sentinel-boot/src/main/java/com/maxzuo/controller/Rest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Created by zfh on 2019/05/30
  */
@@ -16,12 +18,20 @@ public class Rest {
 
     private final static Logger logger = LoggerFactory.getLogger(Rest.class);
 
+    private final AtomicInteger count = new AtomicInteger(1);
+
     @Autowired
     private IShopOrderInfoService shopOrderInfoService;
 
     @GetMapping("queryShopInfo")
-    public void queryShopInfo () {
-        ShopOrderInfo orderInfo = shopOrderInfoService.getShopOrderInfoByPrimaryKey(10);
-        System.out.println("orderInfo: " + orderInfo);
+    public String queryShopInfo () {
+        try {
+            shopOrderInfoService.getShopOrderInfoByPrimaryKey(10);
+            System.out.println("count: " + count.getAndIncrement());
+        } catch (Exception e) {
+            logger.info("发生异常：{}", e.getMessage());
+            return "error";
+        }
+        return "ok";
     }
 }
