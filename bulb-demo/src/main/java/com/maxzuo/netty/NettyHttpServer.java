f7d5e393108388983a14fe170049c04f9d17a483
zfh <fuhongzuo@gmail.com>
2019-07-03 23:27:53 +0800

    新增Netty http server

---
+++ bulb-demo/src/main/java/com/maxzuo/netty/NettyHttpServer.java
@@ -0,0 +1,76 @@
+package com.maxzuo.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.*;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Netty实现简单的Http服务器
+ * <p>
+ * Created by zfh on 2019/07/03
+ */
+public class NettyHttpServer {
+
+    public static void main(String[] args) {
+        int port = 8080;
+        new NettyHttpServer().run(port);
+    }
+
+    public void run(int port) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        System.out.println("准备运行端口：" + port);
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    public void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(
+                                new HttpResponseEncoder(),
+                                new HttpRequestDecoder(),
+                                new ChannelInboundHandlerAdapter() {
+                                    @Override
+                                    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                        FullHttpResponse response = new DefaultFullHttpResponse(
+                                                HttpVersion.HTTP_1_1,
+                                                HttpResponseStatus.OK,
+                                                Unpooled.wrappedBuffer("Welcome to the netty http server !".getBytes(StandardCharsets.UTF_8)));
+                                        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain;charset=UTF-8");
+                                        response.headers().set(HttpHeaderNames.CONTENT_LENGTH, response.content().readableBytes());
+                                        response.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
+                                        ctx.write(response);
+                                        ctx.flush();
+                                    }
+
+                                    @Override
+                                    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+                                        ctx.flush();
+                                    }
+
+                                    @Override
+                                    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+                                        ctx.close();
+                                        cause.printStackTrace();
+                                    }
+                                });
+                    }
+                }).option(ChannelOption.SO_BACKLOG, 128)
+                .childOption(ChannelOption.SO_KEEPALIVE, true);
+        try {
+            ChannelFuture f = bootstrap.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
