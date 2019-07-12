d27d1d283e927b33edf0c15696cf7bd7fbd81622
zfh <fuhongzuo@gmail.com>
2019-07-12 09:21:36 +0800

    内部仅定义资源，外部定义规则

--- bulb-web/src/main/java/com/maxzuo/protect/SentinelRestControllerProtect.java
+++ bulb-web/src/main/java/com/maxzuo/protect/SentinelRestControllerProtect.java
@@ -13,11 +13,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 /**
- * Rest controller层接口保护
+ * Rest controller层资源定义
  * <p>
  * Created by zfh on 2018/09/19
  */
@@ -27,8 +24,6 @@ public class SentinelRestControllerProtect {
 
     private static final Logger logger = LoggerFactory.getLogger(SentinelRestControllerProtect.class);
 
-    private static final ExecutorService rulesPool = Executors.newSingleThreadExecutor();
-
     @Around("execution(* com.maxzuo.controller..*(..))")
     public Object entrance (ProceedingJoinPoint joinPoint) throws Throwable {
         String application = System.getProperty("appname");
@@ -37,10 +32,7 @@ public class SentinelRestControllerProtect {
             String resourceName = getResourceName(joinPoint);
             // 调用链的入口
             ContextUtil.enter(resourceName, application);
-            if (resourceWhilteListFilter(resourceName)) {
-                logger.info("【Sentinel保护资源】resourceName = {}", resourceName);
-                entry = SphU.entry(resourceName, EntryType.IN, 1, joinPoint.getArgs());
-            }
+            entry = SphU.entry(resourceName, EntryType.IN, 1, joinPoint.getArgs());
             return joinPoint.proceed();
         } catch (Throwable t) {
             logger.error("【Sentinel保护资源】发生异常！", t);
@@ -78,33 +70,5 @@ public class SentinelRestControllerProtect {
         }
         resourceName.append(")");
         return resourceName.toString();
-    }
-
-    /**
-     * 白名单资源过滤（不需要保护）
-     * @param resourceName 资源名
-     */
-    private boolean resourceWhilteListFilter (String resourceName) {
-        // TODO: 通过资源名匹配，匹配成功返回false
-        return true;
-    }
-
-    /**
-     * 异步初始化预定义规则
-     * @param resourceName 资源名
-     */
-    private void asyncInitResourcePredefinedRules (String resourceName) {
-        rulesPool.submit(new Runnable() {
-            @Override
-            public void run() {
-                // TODO: 检查资源是否有规则，没有，就插入新规则
-
-            }
-        });
-    }
-
-    @Around("execution(* com.maxzuo.bulb.api..*(..))")
-    public Object dubboEntrance (ProceedingJoinPoint joinPoint) throws Throwable {
-        return joinPoint.proceed();
     }
 }
