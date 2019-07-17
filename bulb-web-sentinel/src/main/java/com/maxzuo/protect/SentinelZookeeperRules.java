3c3fa31c548de8d5056cd99583a9b51738865fc1
zfh <fuhongzuo@gmail.com>
2019-07-17 17:57:51 +0800

    调整zookeeper地址

--- bulb-web-sentinel/src/main/java/com/maxzuo/protect/SentinelZookeeperRules.java
+++ bulb-web-sentinel/src/main/java/com/maxzuo/protect/SentinelZookeeperRules.java
@@ -8,7 +8,6 @@ import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -22,28 +21,29 @@ import java.util.List;
 @Component
 public class SentinelZookeeperRules {
 
-    @Value("${zookeeper.address}")
-    private String zookeeperAddress;
-
     private static final String FLOW_RULE_PATH = "/sentinel_rules/%s/%s/flow";
 
     private static final String DEGRADE_RULE_PATH = "/sentinel_rules/%s/%s/degrade";
 
-    private static final String ENV = "37test";
-
-    private static final String FINAL_NAME = "restful";
-
     @PostConstruct
     public void init() {
+        String zookeeperAddress = "127.0.0.1:2181";
+
+        String env = "37test";
+
+        String finalName = "restful";
+
+        System.setProperty("env", env);
+
         System.out.println("【Sentinel保护资源】开始检出zookeeper规则 ...");
         // 限流规则-Qos
-        String flowPath2 = String.format(FLOW_RULE_PATH, ENV, FINAL_NAME);
+        String flowPath2 = String.format(FLOW_RULE_PATH, env, finalName);
         ReadableDataSource<String, List<FlowRule>> flowRuleDataSource2 = new ZookeeperDataSource<>(zookeeperAddress, flowPath2,
                 source -> JSON.parseObject(source, new TypeReference<List<FlowRule>>() {}));
         FlowRuleManager.register2Property(flowRuleDataSource2.getProperty());
 
         // 降级规则-RT
-        String degradePath = String.format(DEGRADE_RULE_PATH, ENV, FINAL_NAME);
+        String degradePath = String.format(DEGRADE_RULE_PATH, env, finalName);
         ReadableDataSource<String, List<DegradeRule>> degradeDataSource = new ZookeeperDataSource<>(zookeeperAddress, degradePath,
                 source -> JSON.parseObject(source, new TypeReference<List<DegradeRule>>() {}));
         DegradeRuleManager.register2Property(degradeDataSource.getProperty());
