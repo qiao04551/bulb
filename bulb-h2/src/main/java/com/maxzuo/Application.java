2a0ed4e420437e9121c01307f405550d82438b8b
zfh <fuhongzuo@gmail.com>
2019-08-01 22:23:35 +0800

    新增H2内存数据库使用示例

---
+++ bulb-h2/src/main/java/com/maxzuo/Application.java
@@ -0,0 +1,15 @@
+package com.maxzuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zfh on 2019/08/01
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
