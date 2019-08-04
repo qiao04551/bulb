4351f56511ba28b471ee4e7aa7af6237f0cc0d5e
zfh <fuhongzuo@gmail.com>
2019-08-04 17:43:20 +0800

    字符串格式化

---
+++ bulb-demo/src/main/java/com/maxzuo/basic/StringFormatExample.java
@@ -0,0 +1,44 @@
+package com.maxzuo.basic;
+
+/**
+ * 字符串格式化
+ * <p>
+ * Created by zfh on 2019/08/04
+ */
+public class StringFormatExample {
+
+    /**
+     * %s 字符串类型
+     * %c 字符类型
+     * %b 布尔类型
+     * %d 整数类型（十进制）
+     * %x 整数类型（十六进制）
+     * %o 整数类型（八进制）
+     * %f 浮点类型
+     * %a 十六进制浮点类型
+     * %e 指数类型
+     * %g 通用浮点类型（f和e类型中较短的）
+     * %h 散列码
+     * %% 百分比类型
+     * %n 换行符
+     * %tx 日期与时间类型（x代表不同的日期与时间转换符
+     */
+    public static void main(String[] args) {
+        String str;
+        str=String.format("Hi,%s", "王力");
+        System.out.println(str);
+        str=String.format("Hi,%s:%s.%s", "王南","王力","王张");
+        System.out.println(str);
+        System.out.printf("字母a的大写是：%c %n", 'A');
+        System.out.printf("3>7的结果是：%b %n", 3>7);
+        System.out.printf("100的一半是：%d %n", 100/2);
+        System.out.printf("100的16进制数是：%x %n", 100);
+        System.out.printf("100的8进制数是：%o %n", 100);
+        System.out.printf("50元的书打8.5折扣是：%f 元%n", 50*0.85);
+        System.out.printf("上面价格的16进制数是：%a %n", 50*0.85);
+        System.out.printf("上面价格的指数表示：%e %n", 50*0.85);
+        System.out.printf("上面价格的指数和浮点数结果的长度较短的是：%g %n", 50*0.85);
+        System.out.printf("上面的折扣是%d%% %n", 85);
+        System.out.printf("字母A的散列码是：%h %n", 'A');
+    }
+}
