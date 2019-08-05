c90978d6de53f7cd6dd593fc777071654f014328
zfh <fuhongzuo@gmail.com>
2019-08-05 20:05:58 +0800

    CyclicBarrier使用示例

--- bulb-demo/src/main/java/com/maxzuo/juc/CyclicBarrierExample.java
+++ bulb-demo/src/main/java/com/maxzuo/juc/CyclicBarrierExample.java
@@ -4,33 +4,46 @@ import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
+ * CyclicBarrier的使用
+ * <p>
  * Created by zfh on 2019/02/24
  */
 public class CyclicBarrierExample {
 
-    /** 请求总数 */
-    private final static Integer CLIENT_TOTAL  = 100;
+    /**
+     * 请求总数
+     */
+    private final static Integer CLIENT_TOTAL = 30;
 
     private static CyclicBarrier cyclicBarrier = new CyclicBarrier(10);
 
+    private final static AtomicInteger count = new AtomicInteger(1);
+
     public static void main(String[] args) {
         ExecutorService threadPool = Executors.newCachedThreadPool(new NamedThreadFactory());
+
         for (int i = 0; i < CLIENT_TOTAL; i++) {
-            int threadNum = i;
             threadPool.execute(new Runnable() {
                 @Override
                 public void run() {
                     try {
-                        race(threadNum);
+                        race(count.getAndIncrement());
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
             });
         }
-        threadPool.shutdown();
+
+        // threadPool.shutdown();
+        try {
+            Thread.sleep(Integer.MAX_VALUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -38,10 +51,9 @@ public class CyclicBarrierExample {
      * 和CountDownLatch很相识，当计数器达到了设置的初始值时，等待的线程都将被唤醒继续执行。
      * CountDownLatch计数器只能使用一次，而CyclicBarrier是循环执行的，一组接一组的执行。
      */
-    private static void race(Integer threadNum) throws InterruptedException, BrokenBarrierException {
-        Thread.sleep(1000);
-        System.out.println(threadNum + " is readly");
+    private static void race(Integer sleepTime) throws InterruptedException, BrokenBarrierException {
+        Thread.sleep(sleepTime * 500);
         cyclicBarrier.await();
-        System.out.println(threadNum + "done!");
+        System.out.println(Thread.currentThread().getId() + " done!" + sleepTime);
     }
 }
