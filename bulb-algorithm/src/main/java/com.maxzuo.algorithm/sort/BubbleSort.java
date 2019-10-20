d956adaa56877521f76438d6d9277ed4d39b3a69
zfh <fuhongzuo@gmail.com>
2019-10-20 18:10:33 +0800

    冒泡排序，O(n2) 的时间复杂度

---
+++ bulb-algorithm/src/main/java/com.maxzuo.algorithm/sort/BubbleSort.java
@@ -0,0 +1,26 @@
+package com.maxzuo.algorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * 冒泡排序，O(n2) 的时间复杂度
+ * <p>
+ * Created by zfh on 2019/10/20
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] array = {5, 1, 7, 3, 4, 2, 8, 6};
+        for (int i = 0; i < array.length -1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                int tmp;
+                if (array[j] > array[j + 1]) {
+                    tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
