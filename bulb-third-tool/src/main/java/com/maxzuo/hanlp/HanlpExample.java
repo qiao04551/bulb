52cc3698e1165992d3b98703f4a59b45eb49820b
zfh <fuhongzuo@gmail.com>
2019-11-05 22:50:49 +0800

    HanLP是由一系列模型与算法组成的工具包

---
+++ bulb-third-tool/src/main/java/com/maxzuo/hanlp/HanlpExample.java
@@ -0,0 +1,149 @@
+package com.maxzuo.hanlp;
+
+import com.hankcs.hanlp.HanLP;
+import com.hankcs.hanlp.dictionary.py.Pinyin;
+import com.hankcs.hanlp.seg.common.Term;
+import com.hankcs.hanlp.suggest.Suggester;
+import com.hankcs.hanlp.tokenizer.StandardTokenizer;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * HanLP是由一系列模型与算法组成的工具包
+ * <pre>
+ *   hankcs/HanLP：https://github.com/hankcs/HanLP
+ * </pre>
+ * <p>
+ * Created by zfh on 2019/11/05
+ */
+public class HanlpExample {
+
+    public static void main(String[] args) {
+        System.out.println(HanLP.segment("你好，欢迎使用HanLP汉语处理包！"));
+    }
+
+    /**
+     * 标准分词器
+     */
+    @Test
+    public void testStandardTokenizer () {
+        List<Term> termList = StandardTokenizer.segment("商品和服务");
+        System.out.println(termList);
+    }
+
+    /**
+     * 短语提取
+     */
+    @Test
+    public void testExtractPhrase () {
+        String text = "算法工程师\n" +
+                "算法（Algorithm）是一系列解决问题的清晰指令，也就是说，能够对一定规范的输入，在有限时间内获得所要求的输出。如果一个算法有缺陷，或不适合于某个问题，执行这个算法将不会解决这个问题。不同的算法可能用不同的时间、空间或效率来完成同样的任务。一个算法的优劣可以用空间复杂度与时间复杂度来衡量。算法工程师就是利用算法处理事物的人。\n" +
+                "\n" +
+                "1职位简介\n" +
+                "算法工程师是一个非常高端的职位；\n" +
+                "专业要求：计算机、电子、通信、数学等相关专业；\n" +
+                "学历要求：本科及其以上的学历，大多数是硕士学历及其以上；\n" +
+                "语言要求：英语要求是熟练，基本上能阅读国外专业书刊；\n" +
+                "必须掌握计算机相关知识，熟练使用仿真工具MATLAB等，必须会一门编程语言。\n" +
+                "\n" +
+                "2研究方向\n" +
+                "视频算法工程师、图像处理算法工程师、音频算法工程师 通信基带算法工程师\n" +
+                "\n" +
+                "3目前国内外状况\n" +
+                "目前国内从事算法研究的工程师不少，但是高级算法工程师却很少，是一个非常紧缺的专业工程师。算法工程师根据研究领域来分主要有音频/视频算法处理、图像技术方面的二维信息算法处理和通信物理层、雷达信号处理、生物医学信号处理等领域的一维信息算法处理。\n" +
+                "在计算机音视频和图形图像技术等二维信息算法处理方面目前比较先进的视频处理算法：机器视觉成为此类算法研究的核心；另外还有2D转3D算法(2D-to-3D conversion)，去隔行算法(de-interlacing)，运动估计运动补偿算法(Motion estimation/Motion Compensation)，去噪算法(Noise Reduction)，缩放算法(scaling)，锐化处理算法(Sharpness)，超分辨率算法(Super Resolution),手势识别(gesture recognition),人脸识别(face recognition)。\n" +
+                "在通信物理层等一维信息领域目前常用的算法：无线领域的RRM、RTT，传送领域的调制解调、信道均衡、信号检测、网络优化、信号分解等。\n" +
+                "另外数据挖掘、互联网搜索算法也成为当今的热门方向。\n" +
+                "算法工程师逐渐往人工智能方向发展。";
+        List<String> phraseList = HanLP.extractPhrase(text, 10);
+        System.out.println(phraseList);
+    }
+
+    /**
+     * 汉字转拼音
+     */
+    @Test
+    public void testPinyin () {
+        String text = "重载不是重任";
+        List<Pinyin> pinyinList = HanLP.convertToPinyinList(text);
+        System.out.print("原文,");
+        for (char c : text.toCharArray())
+        {
+            System.out.printf("%c,", c);
+        }
+        System.out.println();
+
+        System.out.print("拼音（数字音调）,");
+        for (Pinyin pinyin : pinyinList)
+        {
+            System.out.printf("%s,", pinyin);
+        }
+        System.out.println();
+
+        System.out.print("拼音（符号音调）,");
+        for (Pinyin pinyin : pinyinList)
+        {
+            System.out.printf("%s,", pinyin.getPinyinWithToneMark());
+        }
+        System.out.println();
+
+        System.out.print("拼音（无音调）,");
+        for (Pinyin pinyin : pinyinList)
+        {
+            System.out.printf("%s,", pinyin.getPinyinWithoutTone());
+        }
+        System.out.println();
+
+        System.out.print("声调,");
+        for (Pinyin pinyin : pinyinList)
+        {
+            System.out.printf("%s,", pinyin.getTone());
+        }
+        System.out.println();
+
+        System.out.print("声母,");
+        for (Pinyin pinyin : pinyinList)
+        {
+            System.out.printf("%s,", pinyin.getShengmu());
+        }
+        System.out.println();
+
+        System.out.print("韵母,");
+        for (Pinyin pinyin : pinyinList)
+        {
+            System.out.printf("%s,", pinyin.getYunmu());
+        }
+        System.out.println();
+
+        System.out.print("输入法头,");
+        for (Pinyin pinyin : pinyinList)
+        {
+            System.out.printf("%s,", pinyin.getHead());
+        }
+        System.out.println();
+    }
+
+    /**
+     * 文本推荐(句子级别，从一系列句子中挑出与输入句子最相似的那一个)
+     */
+    @Test
+    public void testSuggester () {
+        Suggester suggester = new Suggester();
+        String[] titleArray =
+                (
+                        "威廉王子发表演说 呼吁保护野生动物\n" +
+                                "《时代》年度人物最终入围名单出炉 普京马云入选\n" +
+                                "“黑格比”横扫菲：菲吸取“海燕”经验及早疏散\n" +
+                                "日本保密法将正式生效 日媒指其损害国民知情权\n" +
+                                "英报告说空气污染带来“公共健康危机”"
+                ).split("\\n");
+        for (String title : titleArray)
+        {
+            suggester.addSentence(title);
+        }
+        System.out.println(suggester.suggest("发言", 1));       // 语义
+        System.out.println(suggester.suggest("危机公共", 1));   // 字符
+        System.out.println(suggester.suggest("mayun", 1));      // 拼音
+    }
+}
