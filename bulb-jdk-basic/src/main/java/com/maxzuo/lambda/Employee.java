04f63b74b72af131591c484d647ec33a66bacf92
zfh <fuhongzuo@gmail.com>
2019-11-13 12:55:39 +0800

    Lambda表达式

---
+++ bulb-jdk-basic/src/main/java/com/maxzuo/lambda/Employee.java
@@ -0,0 +1,16 @@
+package com.maxzuo.lambda;
+
+/**
+ * Created by zfh on 2019/11/13
+ */
+public class Employee {
+
+    public String name = "dazuo";
+
+    public Employee () {
+    }
+
+    public Employee(String name) {
+        name = name;
+    }
+}
