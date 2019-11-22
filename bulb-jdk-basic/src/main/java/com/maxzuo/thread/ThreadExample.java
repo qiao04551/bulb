16738a1288e1773173aaa2186b641f894ec27f90
zfh <fuhongzuo@gmail.com>
2019-11-22 09:43:36 +0800

    Thread#interrupt()方法整理

--- bulb-jdk-basic/src/main/java/com/maxzuo/thread/ThreadExample.java
+++ bulb-jdk-basic/src/main/java/com/maxzuo/thread/ThreadExample.java
@@ -130,7 +130,7 @@ class ThreadExample {
     }
 
     /**
-     * Thread#interrupt() 和 Thread.interrupted()
+     * Thread#interrupt()
      * <pre>
      *   1.如果线程处于正常活动状态，那么会将该线程的中断标志设置为 true，仅此而已。被设置中断标志的线程将继续正常运行，不受影响。
      *   2.如果线程处于被阻塞状态（如线程调用了thread.sleep、thread.join、thread.wait、1.5中的condition.await、以及可中断
@@ -144,27 +144,15 @@ class ThreadExample {
      * 	    2）将当前线程的中断状态设为false.
      * </pre>
      */
+    @DisplayName("阻塞中的线程")
     @Test
-    void testThreadInterruptMethod () throws InterruptedException {
-        methodTwo();
-        System.out.println("main end ...");
-        TimeUnit.SECONDS.sleep(Long.MAX_VALUE);
-    }
-
-    /**
-     * 如果线程处于被阻塞状态（如线程调用了thread.sleep、thread.join、thread.wait、1.5中的condition.await、
-     * 以及可中断的通道上的 I/O 操作方法后可进入阻塞状态），则会在这些阻塞方法调用处抛出InterruptedException异常，
-     * 并且在抛出异常后立即将线程的中断标示位清除，即重新设置为false。抛出异常是为了线程从阻塞状态醒过来，并在结束
-     * 线程前让程序员有足够的时间来处理中断请求。
-     */
-    private static void methodOne() throws InterruptedException {
+    void testThreadInterrupt() throws InterruptedException {
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println("t1 start ...");
                 try {
-                    TimeUnit.SECONDS.sleep(5);
-                    System.out.println("t1 end ...");
+                    System.out.println("t1 start ...");
+                    TimeUnit.SECONDS.sleep(2);
                 } catch (InterruptedException e) {
                     // 输出：--t1-- RUNNABLE
                     System.out.println(Thread.currentThread().getName() + " " + Thread.currentThread().getState());
@@ -174,16 +162,17 @@ class ThreadExample {
         }, "--t1--");
         t1.start();
 
-        TimeUnit.SECONDS.sleep(2);
-        // 输出：RUNNABLE
+        TimeUnit.SECONDS.sleep(1);
+
         System.out.println("t1 state：" + t1.getState());
         t1.interrupt();
+
+        TimeUnit.SECONDS.sleep(2);
     }
 
-    /**
-     * 如果线程处于正常活动状态，那么会将该线程的中断标志设置为 true，仅此而已。被设置中断标志的线程将继续正常运行，不受影响。
-     */
-    private static void methodTwo() throws InterruptedException {
+    @DisplayName("运行中的线程")
+    @Test
+    void methodTwo() throws InterruptedException {
         Thread t2 = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -194,7 +183,6 @@ class ThreadExample {
                 }
                 // 输出：RUNNABLE
                 System.out.println("t2 state：" + Thread.currentThread().getState());
-                System.out.println("t2 continue ...");
 
                 /**
                  * Thread.interrupted()会做两步操作：
