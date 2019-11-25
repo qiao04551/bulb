fe71d96d68269c6e6b434d8099abc8de89f95493
zfh <fuhongzuo@gmail.com>
2019-11-25 16:20:50 +0800

    DateUtils的使用

--- bulb-jdk-basic/src/main/java/com/maxzuo/utils/DateUtilsTest.java
+++ bulb-jdk-basic/src/main/java/com/maxzuo/utils/DateUtilsTest.java
@@ -11,25 +11,42 @@ import java.util.Date;
  * Apache DateUtils的使用
  * Created by zfh on 2019/01/05
  */
-public class DateUtilsTest {
+class DateUtilsTest {
 
     /**
-     * 解析日期时间字符串日期时间Date对象
+     * 解析日期时间字符串
      */
     @Test
-    public void testParseDate() throws ParseException {
+    void testParseDate() throws ParseException {
         // 解析时间
         Date date = DateUtils.parseDate("2019-1-5 8:30:20:222", "yyyy-MM-dd HH:mm:ss:SSS");
+        System.out.println(date);
+
+        Date date1 = DateUtils.parseDate("2019-11-25", "yyyy-MM-dd");
+        System.out.println(date1);
+    }
+
+    /**
+     * 日期格式化
+     */
+    @Test
+    void testDateFormat () {
         // 格式化时间
-        String formatTime = DateFormatUtils.format(date, "yyyy-MM-dd HH:mm:ss:SSS");
+        String formatTime = DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss:SSS");
         System.out.println("formatTime: " + formatTime);
+
+        String dateStr = DateFormatUtils.format(new Date(), "yyyy-MM-dd");
+        System.out.println("dateStr: " + dateStr);
+
+        String timeStr = DateFormatUtils.format(new Date(), "HH:mm");
+        System.out.println("timeStr: " + timeStr);
     }
 
     /**
      * 对日期的加减
      */
     @Test
-    public void test() {
+    void testCaculateDate() {
         Date date = DateUtils.addYears(new Date(), 1);
         DateUtils.addMonths(new Date(), 1);
         DateUtils.addDays(new Date(), 1);
