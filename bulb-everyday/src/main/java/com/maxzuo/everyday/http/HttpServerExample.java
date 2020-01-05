6234c273b3a2b9fdbd3c8d00a4ace16d06e56f4c
zfh <fuhongzuo@gmail.com>
2020-01-05 18:16:02 +0800

    JDK内置的轻量级Http服务器

---
+++ bulb-everyday/src/main/java/com/maxzuo/everyday/http/HttpServerExample.java
@@ -0,0 +1,43 @@
+package com.maxzuo.everyday.http;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Executors;
+
+/**
+ * JDK内置的轻量级Http服务器
+ * <p>
+ * Created by zfh on 2020/01/05
+ */
+public class HttpServerExample {
+
+    public static void main(String[] args) throws IOException {
+        createHttpServer();
+    }
+
+    private static void createHttpServer() throws IOException {
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress(8080), 0);
+        httpServer.setExecutor(Executors.newCachedThreadPool());
+        httpServer.createContext("/", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange httpExchange) throws IOException {
+                byte[] data = "hello world" .getBytes(StandardCharsets.UTF_8);
+                httpExchange.sendResponseHeaders(200, data.length);
+                OutputStream outputStream = httpExchange.getResponseBody();
+                outputStream.write(data);
+                outputStream.close();
+                httpExchange.close();
+            }
+        });
+        httpServer.createContext("/time", new TimeHandler());
+        httpServer.start();
+
+        // httpServer.stop(0);
+    }
+}
---
+++ bulb-everyday/src/main/java/com/maxzuo/everyday/http/TimeHandler.java
@@ -0,0 +1,25 @@
+package com.maxzuo.everyday.http;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+
+/**
+ * Created by zfh on 2020/01/05
+ */
+public class TimeHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        byte[] data = ("time " + LocalDateTime.now().toString()).getBytes(StandardCharsets.UTF_8);
+        httpExchange.sendResponseHeaders(200, data.length);
+        OutputStream out = httpExchange.getResponseBody();
+        out.write(data);
+        out.close();
+        httpExchange.close();
+    }
+}
