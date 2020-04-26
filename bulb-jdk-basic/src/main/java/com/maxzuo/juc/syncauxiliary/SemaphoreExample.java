590a3a56380f3754d0770715784cbe04591d8728
zfh <fuhongzuo@gmail.com>
2020-04-26 17:54:03 +0800

    信号量类-示例调整

--- bulb-jdk-basic/src/main/java/com/maxzuo/juc/syncauxiliary/SemaphoreExample.java
+++ bulb-jdk-basic/src/main/java/com/maxzuo/juc/syncauxiliary/SemaphoreExample.java
@@ -1,29 +1,33 @@
 package com.maxzuo.juc.syncauxiliary;
 
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 同步辅助类-信号量Semaphore
+ * <p>
  * Created by zfh on 2019/02/24
  */
 public class SemaphoreExample {
 
-    /** 请求总数 */
+    /**
+     * 请求总数
+     */
     private final static Integer CLIENT_TOTAL = 1000;
 
-    /** 同时并发执行的线程数 */
+    /**
+     * 同时并发执行的线程数
+     */
     private final static Integer THREAD_TOTAL = 100;
 
-    private static Integer       count        = 0;
+    private static AtomicInteger count = new AtomicInteger();
 
     public static void main(String[] args) {
         ExecutorService threadPool = Executors.newCachedThreadPool();
 
         final Semaphore semaphore = new Semaphore(THREAD_TOTAL);
-        CountDownLatch countDownLatch = new CountDownLatch(CLIENT_TOTAL);
         for (int i = 0; i < CLIENT_TOTAL; i++) {
             threadPool.execute(() -> {
                 try {
@@ -34,23 +38,13 @@ public class SemaphoreExample {
                     semaphore.release();
                 } catch (Exception e) {
                     e.printStackTrace();
-                } finally {
-                    countDownLatch.countDown();
                 }
             });
-        }
-        try {
-            countDownLatch.await();
-            System.out.println("count: " + count);
-            threadPool.shutdown();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
     }
 
     private static void add() throws InterruptedException {
-        ++count;
-        System.out.println(Thread.currentThread().getName() + " i = " + count);
-        //Thread.sleep(1000);
+        System.out.println(Thread.currentThread().getName() + " i = " + count.incrementAndGet());
+        Thread.sleep(1000);
     }
 }
