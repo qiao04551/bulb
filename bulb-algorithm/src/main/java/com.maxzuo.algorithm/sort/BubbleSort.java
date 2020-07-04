76422e2d6d8dead8ddf80e794e08ff893deb1373
zfh <fuhongzuo@gmail.com>
2020-07-04 16:47:47 +0800

    冒泡排序

--- bulb-algorithm/src/main/java/com.maxzuo.algorithm/sort/BubbleSort.java
+++ bulb-algorithm/src/main/java/com.maxzuo.algorithm/sort/BubbleSort.java
@@ -4,7 +4,10 @@ import java.util.Arrays;
 
 /**
  * 冒泡排序，O(n^2) 的时间复杂度，O（1）空间复杂度
- * <p>
+ * <pre>
+ *   1.从当前元素起，向后依次比较每一对相邻元素，若逆序则交换
+ *   2.对所有元素均重复以上步骤，直至最后一个元素。
+ * </pre>
  * Created by zfh on 2019/10/20
  */
 public class BubbleSort {
