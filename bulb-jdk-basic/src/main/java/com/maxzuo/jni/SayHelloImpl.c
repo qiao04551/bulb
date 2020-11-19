c5c0e15ccb0ee4a0109074971c04ff89333abe1c
zfh <fuhongzuo@gmail.com>
2020-11-19 15:51:13 +0800

    抽离JNI

--- bulb-jdk-basic/src/main/java/com/maxzuo/jni/SayHelloImpl.c
+++
@@ -1,8 +0,0 @@
-#include "SayHello.h"
-#include <stdio.h>
-
-// C函数是用下划线(_)作为命名分隔（对应java的包名）
-JNIEXPORT void JNICALL Java_com_maxzuo_jni_SayHello_sayHello(JNIEnv *env,jobject obj){
-    printf("Hello JNI\n");
-    return;
-}
